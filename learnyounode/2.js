9b446b7f8d54d0e4e6d192b91d29851ff2f01198
Nedeljko Tadic <tadicn@gmail.com>
2017-12-05 11:27:20 +0100

    Added exercise text. Code formatting.

--- learnyounode/2.js
+++ learnyounode/2.js
@@ -1,7 +1,10 @@
+/* ## BABY STEPS (Exercise 2 of 13)
+
+Write a program that accepts one or more numbers as command-line arguments
+and prints the sum of those numbers to the console (stdout). */
 console.log(process.argv.reduce((acc, curr, index) => {
-    if(index > 1){
-      return acc + Number(curr)
-    }
-    return 0
-  }, 0)
-)
+  if (index > 1) {
+    return acc + Number(curr)
+  }
+  return 0
+}, 0))
--- learnyounode/3.js
+++ learnyounode/3.js
@@ -1,3 +1,12 @@
+/* ## MY FIRST I/O! (Exercise 3 of 13)
+
+Write a program that uses a single synchronous filesystem operation to
+read a file and print the number of newlines (\n) it contains to the
+console (stdout), similar to running cat file | wc -l.
+
+The full path to the file to read will be provided as the first
+command-line argument (i.e., process.argv[2]). You do not need to make
+your own test file. */
 const fs = require('fs')
 let buff = fs.readFileSync(process.argv[2])
 console.log(buff.toString().split('\n').length - 1)
